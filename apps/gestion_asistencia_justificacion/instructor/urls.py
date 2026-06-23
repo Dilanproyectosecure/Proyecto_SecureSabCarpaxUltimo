1e15eee73a5d19b218f52cece8a9406f74c191b2
Dilan <dfrancod2@soy.sena.edu.co>
2026-06-23 08:24:57 -0500

    Implementacion full

--- apps/gestion_asistencia_justificacion/instructor/urls.py
+++ apps/gestion_asistencia_justificacion/instructor/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('notificar-aprendiz/', views.notificar_aprendiz, name='notificar_aprendiz'),
     path('enviar-correos-inasistencia/', views.enviar_correos_inasistencia_view, name='enviar_correos_inasistencia'),
     path('enviar-correo-retardo/', views.enviar_correo_retardo_view, name='enviar_correo_retardo'),
+    path('dismiss-llamado/<int:llamado_id>/', views.dismiss_llamado, name='dismiss_llamado'),
 ]
